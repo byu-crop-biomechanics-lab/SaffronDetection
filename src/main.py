c82d7b7681c2574e9fd526ffe1a06ace8c83ca17
Nathan Jones <nathanij@byu.edu>
2023-07-25 11:00:58 -0600

    data img edit

--- src/main.py
+++ src/main.py
@@ -263,7 +263,7 @@ class CameraColorApp(App):
                     
                     if view_name == "data":
                         
-                        img = np.zeros((500, 500, 3), dtype = np.uint8)
+                        img = np.zeros((500, 500, 3), np.uint8)
 
                         # img = self.image_decoder.decode(
                         #     getattr(frame, "rgb").image_data
