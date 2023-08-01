60e9fef5dab049621702a1df85c6bd3980ef5d57
Nathan Jones <nathanij@byu.edu>
2023-08-01 15:27:55 -0600

    edit

--- src/main.py
+++ src/main.py
@@ -405,7 +405,7 @@ class CameraColorApp(App):
         
         #-------Data-------#
         data_img = 20 * np.ones(shape=[800, 1000, 3], dtype=np.uint8)
-        cv2.putText(data_img, str(self.oaks.streams[0].has()), (30,150),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        cv2.putText(data_img, str(self.streams[0].has()), (30,150),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
 
         texture = Texture.create(
