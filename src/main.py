d6b4e97a6631b552cf771cfe4ef87daa71ac7074
Nathan Jones <nathanij@byu.edu>
2023-08-01 16:18:40 -0600

    edit

--- src/main.py
+++ src/main.py
@@ -393,6 +393,7 @@ class CameraColorApp(App):
         camRgb.setBoardSocket(dai.CameraBoardSocket.CAM_A)
         camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
         camRgb.setVideoSize(1920, 1080)
+        camRgb.setFps(5)
 
         # Create output
         xoutRgb.input.setBlocking(False)
