fbb9de4317dbe1cb9c8aeb80f57b586fa8d11718
Nathan Jones <nathanij@byu.edu>
2023-08-01 09:38:39 -0600

    edit

--- src/main.py
+++ src/main.py
@@ -358,7 +358,7 @@ class CameraColorApp(App):
         while self.root is None:
             await asyncio.sleep(0.01)
             
-        img = 200 * np.ones(shape=[512, 512, 3], dtype=np.uint8)
+        img = 200 * np.ones(shape=[1000, 512, 3], dtype=np.uint8)
         
         texture = Texture.create(
             size=(img.shape[1], img.shape[0]), icolorfmt="bgr"
@@ -372,7 +372,7 @@ class CameraColorApp(App):
             mipmap_generation=False,
         )
         
-        self.root.ids["rgb"].texture = texture
+        self.root.ids["data"].texture = texture
         
                     
                     
